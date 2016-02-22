876f71cf75594fe26a5640750e057062ce641c1b
auremoser <auremoser@gmail.com>
2016-02-21 22:50:03 -0500

    update globals with my token

--- js/globals.js
+++ js/globals.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var USERNAME = 'mateogianolio', // github username
+var USERNAME = 'auremoser', // github username
     REPO = 'openblog', // github repository
-    TOKEN = '065d9dada1a528b2a741cc5a9699f1be2589644c'; // github app access token
+    TOKEN = 'c2d97c19c90c64783c46c76b2a307d64ebaf45a4'; // github app access token
